adc1b0d60fe1b825d8367498ec742f61932bed29
saumyasahu-7 <saumyasahu.7feb@gmail.com>
2024-08-05 17:20:58 +0530

    Update addpoints_routes.py

--- routes/addpoints_routes.py
+++ routes/addpoints_routes.py
@@ -1,14 +1,24 @@
 from flask import Blueprint, request, jsonify
 from models.person import Student
+from db import mongo
+# from flask_validator import ValidateJson, ValidateInteger, ValidateString
 
-addpoints_bp=Blueprint('addpoints', __name__)
-#API endpoint for adding points to student and their respective house
+addpoints_bp = Blueprint('addpoints', __name__)
+
+def add_points_to_house():
+    pass
+# API endpoint for adding points to a student and their respective house
 @addpoints_bp.route('/addpoints', methods=['PUT'])
 def add_points():
-    if(request.method=='PUT'):
-        data=request.json
-        student_id=data.get('student_id')
-        points_added=data.get('points_added')
-        student=Student.objects(person_id=student_id).first()
-        student.add_points(points_added)
-    return "Points added"
+    data = request.json
+    student_id = data.get('student_id')
+    points_added = data.get('points_added')
+
+    student = mongo.db.students.find({"id":student_id})
+    
+    if not student:
+        return jsonify({"message": "Student not found"}), 404
+
+    
+
+    return jsonify({"message": "Points added successfully", "student_id": student_id, "points_added": points_added}), 200
