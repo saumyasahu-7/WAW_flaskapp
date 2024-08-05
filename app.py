805c476ef616e43c3b9c2ef5929ec42e840a58fe
saumyasahu-7 <saumyasahu.7feb@gmail.com>
2024-08-05 17:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,12 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-from mongoengine import connect, disconnect
-from routes.__init__ import init_app
+from db import app # Import mongo from db
 from models.house import House
+from routes.__init__ import init_app
 
-disconnect()
-connect(db="mydb", alias='default')
+if __name__ == "__main__":
+    house = House("Gryffindor", "h1")
+    house = House("Hufflepuff", "h2")
+    house = House("Ravenclaw", "h3")
+    house = House("Slytherin", "h4")
 
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
-mongo = PyMongo(app)
-init_app(app)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-if(__name__=="__main__"):
-    Gryffindor=House.create_house("Gryffindor","h1")
-    Hufflepuff=House.create_house("Hufflepuff","h2")
-    Ravenclaw=House.create_house("Ravenclaw","h3")
-    Slytherin=House.create_house("Slytherin","h4")
+    init_app(app)
     app.run(debug=True, port=5001)
